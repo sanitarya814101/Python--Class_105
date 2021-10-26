5c12a44fd5cf5980761f23dbb3647a6826798e8e
sanitarya814101 <84065869+sanitarya814101@users.noreply.github.com>
2021-10-26 16:49:13 +0530

    Add files via upload

---
+++ Class1.py
@@ -0,0 +1,34 @@
+import csv
+import pandas as pd
+import plotly.express as px
+
+with open("class1.csv", newline="") as f:
+    reader = csv.reader(f)
+
+    file_data = list(reader)
+
+file_data.pop(0)
+
+total_marks = 0
+total_entries = len(file_data)
+
+
+for marks in file_data:
+    total_marks += float(marks[1])
+
+mean = total_marks/total_entries
+print("The mean value is: " + str(mean))
+
+df = pd.read_csv("class1.csv")
+fig = px.scatter(df, x="Student Number", y="Marks")
+
+fig.update_layout(shapes=[
+    dict(
+        type='line',
+        x0=0,
+        x1=total_entries,
+        y0=mean,
+        y1=mean
+    )
+])
+fig.show()
